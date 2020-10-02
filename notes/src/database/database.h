5e96698cf9236c44939e1f45b04820c344b17c5a
1ss2 <ssrivastava1099@gmail.com>
2020-10-02 17:45:38 +0530

    files

---
+++ notes/src/database/database.h
@@ -0,0 +1,13 @@
+
+#ifndef DATABASE 
+#define DATABASE 
+
+
+
+//int routeId(char*, char* , char* );
+char* transformKey(int );
+char* transportKey(int );
+char* transportValue(int );
+char* transformValue(int );
+
+#endif
